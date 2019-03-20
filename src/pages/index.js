b0d4b71d94d53d0430312ef046ffdb4f24f6bfc3
Thomas Parker <thomas.parker@smartbizloans.com>
2019-03-20 11:21:26 -0700

    changes what is rendered

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,13 @@
 import React from "react"
 
-export default () => <div>Hello world!</div>
+class App extends Component {
+  render() {
+    return (
+     <div>
+        <h1>Googlebot will always crawl</h1>
+      </div>
+    );
+  }
+}
+
+export default App
