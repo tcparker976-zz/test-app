8dd5ad574b51d36f43696e20c993c14ac8469421
Thomas Parker <thomas.parker@smartbizloans.com>
2019-03-20 13:03:11 -0700

    adds the meta tag in component did mount

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,12 @@
 import React, { Component } from "react"
 
 class App extends Component {
+  componentDidMount() {
+    const tag = document.createElement('meta')
+    tag.setAttribute("name", "google-site-verification")
+    tag.setAttribute("content", "bCj3HhIqDlnOUnl8OuWzxcoPCcWXIYLNEI68kALtrKQ")
+    document.head.append(tag)
+  }
   render() {
     return (
      <div>
