6b6d9eacf774b4dfd5c6078ef0e27b8571b7ceca
Thomas Parker <thomas.parker@smartbizloans.com>
2019-03-20 13:56:14 -0700

    adds the gatsby react-helmet plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,8 @@ module.exports = {
   siteMetadata: {
     siteUrl: `https://www.example.com`,
   },
-  plugins: [`gatsby-plugin-sitemap`]
+  plugins: [
+    `gatsby-plugin-sitemap`,
+    'gatsby-plugin-react-helmet'
+  ]
 }
