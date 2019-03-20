bf5bce5425e26892bbbffea9489cebcc5ca32ce8
Thomas Parker <thomas.parker@smartbizloans.com>
2019-03-20 13:33:51 -0700

    adds meta tag with helmet

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,23 @@
-import React, { Component } from "react"
+import React, { Component, Fragment } from "react"
+import Helmet from "react-helmet"
 
 class App extends Component {
-  componentDidMount() {
-    const tag = document.createElement('meta')
-    tag.setAttribute("name", "google-site-verification")
-    tag.setAttribute("content", "bCj3HhIqDlnOUnl8OuWzxcoPCcWXIYLNEI68kALtrKQ")
-    document.head.append(tag)
-  }
   render() {
     return (
-     <div>
+    <Fragment>
+      <Helmet
+        title="Test-Project"
+        meta={[
+          { name: 'description', content: 'Your description here' },
+          { name: 'google-site-verification', content: 'bCj3HhIqDlnOUnl8OuWzxcoPCcWXIYLNEI68kALtrKQ' }
+        ]}
+      />
+      <div>
         <h1>Googlebot will always crawl</h1>
         <p>This is a test</p>
       </div>
+    </Fragment>
+    
     );
   }
 }
