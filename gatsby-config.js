e18fd69a9b8b20efdf74230144dd03dd820986be
Thomas Parker <thomas.parker@smartbizloans.com>
2019-03-21 11:01:47 -0700

    adds sitemap plugin to the project

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,6 +18,7 @@ module.exports = {
     'gatsby-plugin-react-helmet',
     `gatsby-plugin-emotion`,
     `gatsby-transformer-remark`,
+    `gatsby-plugin-sitemap`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
